c61ed756fa28ca2ec1a72e7df257d0f61ac99fbe
Shoghi Cervantes <shoghicp@gmail.com>
2013-09-06 20:42:09 +0200

    API 11

--- src/config.php
+++ src/config.php
@@ -61,7 +61,7 @@ define("LOG", true);
 define("START_TIME", microtime(true));
 define("MAJOR_VERSION", "Alpha_1.3.10dev");
 define("CURRENT_MINECRAFT_VERSION", "0.7.5 alpha");
-define("CURRENT_API_VERSION", 10);
+define("CURRENT_API_VERSION", 11);
 define("CURRENT_PHP_VERSION", "5.5");
 $gitsha1 = false;
 if(file_exists(FILE_PATH.".git/refs/heads/master")){ //Found Git information!
